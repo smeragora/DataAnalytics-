10f570a03c8efc597edfc815ee082bbee4eea0ea
smeragora <40766623+smeragora@users.noreply.github.com>
2018-07-06 11:27:10 -0400

    Update R  coding.R

--- R  coding.R
+++ R  coding.R
@@ -7,21 +7,7 @@ dbl_var <- c(1, 2.5, 4.5)
 dbl_var
 
 
-x<-1:10
-y<-4:5
-q<-c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j")
-theDF = data.frame (x,y,q)
-theDF
-theDF<- data.frame(first=x, second=y, number= q)
-theDF
-nrow(theDF)
-ncol(theDF)
-dim(theDF)
-names(theDF)
-rownames(theDF)
-theArray<-array(1:12, dim=c(2,3,2))
-temp=theDF$first
-temp = theDF[,"first", drop=FALSE]
+
 
 
 
